6b5b9c05264fa2293d3f4d6dceae59f2f2811eaf
electrondevdude <electrondev@protonmail.com>
2022-03-29 22:22:10 +0100

    random image php file

---
+++ assets/images/bg.php
@@ -0,0 +1,25 @@
+<?php
+$path_photos = "/var/www/boko_camp/assets/images/photos";
+$images = array();
+function getFilesIn($directory)
+{
+       foreach (new DirectoryIterator($directory) as $file) {
+              if ($file->isDot())
+              {
+                     continue;
+              }
+              else{
+                     if (is_dir($directory . "/" .  $file->getFilename()))
+                     {
+                            getFilesIn($directory . "/" . $file->getFilename());
+                     }
+                     else{
+                            global $images, $path;
+                            $images[] = str_ireplace($path, "", ($directory . "/" .  $file->getFilename()));
+                     }
+              }
+       }
+}
+getFilesIn($path_photos);
+echo $images[array_rand($images)];
+?>
